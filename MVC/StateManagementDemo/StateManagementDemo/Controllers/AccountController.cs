5be0ea18ea9f8084eb0e006154f1787cf5dabe91
sanket <sankethalake@gmail.com>
2022-05-17 15:25:08 +0530

    TempData and session

---
+++ MVC/StateManagementDemo/StateManagementDemo/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StateManagementDemo.Controllers
+{
+    public class AccountController : Controller
+    {        
+            // GET: Account
+            [HttpGet]
+            public ActionResult Index()
+            {
+                return View();
+            }
+
+
+
+            [HttpPost]
+            public ActionResult Login(string username, string password)
+            {
+                if (username.Equals("acc1") && password.Equals("123"))
+                {
+                    Session["username"] = username;
+                    return View("Success");
+                }
+                else
+                {
+                    ViewBag.error = "Invalid Account";
+                    return View("Index");
+                }
+            }
+
+
+
+            [HttpGet]
+            public ActionResult Logout()
+            {
+                Session.Remove("username");
+                return RedirectToAction("Index");
+            }
+        
+    }
+}
---
+++ MVC/StateManagementDemo/StateManagementDemo/Controllers/TempDemoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StateManagementDemo.Models;
+
+namespace StateManagementDemo.Controllers
+{
+    public class TempDemoController : Controller
+    {
+        // GET: TempDemo
+        public ActionResult Index()
+        {
+            Person pobj = new Person { PId = 400, Address = "Kolhapur", Name = "ankit" };
+            TempData["data"] = pobj;
+            return RedirectToAction("Index", "Result");
+        }
+    }
+}
